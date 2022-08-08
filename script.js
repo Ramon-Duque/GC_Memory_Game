994e9d1e8920335153ef3bca4a2d0d418cf71ace
MakennaLebo <kennalebo@hotmail.com>
2022-08-08 19:33:01 -0400

    Flip-Cards

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ let count = 0;
 function myCounter() {
   document.getElementById("timer").innerHTML = ++count;
 }
+
+//Function to Flip Cards
+const card = document.getElementById("card-container")
+card.addEventListener("click", flipCard);
+
+function flipCard(){
+    card.classList.toggle('flipCard');
+}
