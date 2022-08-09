11c86c758985358ce23ce6f850958cb24d8b71e0
Michael Rodriguez <mmrodriguez593@hotmail.com>
2022-08-08 20:45:37 -0400

    randomizer

--- script.js
+++ script.js
@@ -3,3 +3,38 @@ let count = 0;
 function myCounter() {
   document.getElementById("timer").innerHTML = ++count;
 }
+
+// randomizer array for cards, with img sources for whatever pics we want to use
+
+const cardImages = [
+  {"src": "./images/tajmahal.jpeg"},
+  {"src": "./images/giza.jpeg"},
+  {"src": "./images/statue_of_liberty.jpeg"},
+  {"src": "./images/arcdetriomphe.jpeg"},
+  {"src": "./images/rushmore.jpeg"},
+  {"src": "./images/colosseum.jpeg"},
+]
+
+function App() {
+
+  const [cards, setCards] = useState([])
+  const [turns, setTurns] = useState(0)
+
+  // shuffle cards
+const shuffleCards =()=>{
+  const shuffleCards = [...cardImages, ...cardImages]
+   .sort(( )=> Math.random() - 0.5)
+   .map((card) => ({ ...card, id: Math.random() }))
+
+   setCards(shuffleCards)
+   setTurns(0)
+}
+console.log(cards, turns);
+
+  return(
+    <div className="App">
+      <h1>Memory Card Game</h1>
+      <button onClick={shuffleCards}>New Game</button>
+    </div>
+  );
+};
