a66ab06e2c46a98211888c304ea160cef1d2d50b
Ramon Duque <ramonduke@gmail.com>
2022-08-12 10:04:22 -0400

    function and styling progress

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
-//List of variables
-
-
 // Function to start timer
-let count = 0;
+let counter = 0;
 function myCounter() {
   document.getElementById("timer").innerHTML = ++count;
 }
 
-
-// randomizer array for cards, with img sources for whatever pics we want to use
 
 const cardArray = [
  {
@@ -37,120 +32,99 @@ const cardArray = [
   }
 ]
 
-const memoryGame = document.getElementById('card-container')
-
-
-const grid = document.createElement('section')
-grid.setAttribute('class', 'grid')
-
-
-memoryGame.appendChild(grid)
-
 let memoryGameGrid = cardArray.concat(cardArray)
 memoryGameGrid.sort(() => 0.5 - Math.random());
 
-memoryGameGrid.forEach((item) => {
- 
-  const card = document.createElement('div')
+let firstGuess = '';
+let secondGuess = '';
+let count = 0;
+let previousTarget = null;
+let delay = 1200;
 
-  
-  card.classList.add('card')
+const memoryGame = document.getElementById('card-container');
+const grid = document.createElement('section');
+grid.setAttribute('class', 'grid');
+memoryGame.appendChild(grid);
 
-  
-  card.dataset.name = item.name
+memoryGameGrid.forEach(item => {
+  const { name, img } = item;
 
-  
-  card.style.backgroundImage = `url('${item.img}')`
+  const card = document.createElement('div');
+  card.classList.add('card');
+  card.dataset.name = name;
 
-  
-  grid.appendChild(card)
+  const front = document.createElement('div');
+  front.classList.add('front');
+
+  const back = document.createElement('div');
+  back.classList.add('back');
+  back.style.backgroundImage = `url(${img})`;
+
+  grid.appendChild(card);
+  card.appendChild(front);
+  card.appendChild(back);
 });
 
-memoryGameGrid.forEach((item) => {
- 
-  const card = document.createElement('div')
-  card.classList.add('card')
-  card.dataset.name = item.name
-
-  
-  const front = document.createElement('div')
-  front.classList.add('front')
-
-  
-  const back = document.createElement('div')
-  back.classList.add('back')
-  back.style.backgroundImage = `url(${item.img})`
-
-  
-  grid.appendChild(card)
-  card.appendChild(front)
-  card.appendChild(back)
-})
-
-
-
-
-let counter = 0;
-let firstSelection = "";
-let secondSelection = "";
-
-const cards = document.querySelectorAll(".cards .card");
-cards.forEach((card) => {
-  card.addEventListener("click", () => {
-    card.classList.add("clicked");
-    
-
-    if (counter === 0) {
-      firstSelection = card.getAttribute("monument");
-      counter++;
-    } else {
-      secondSelection = card.getAttribute("monument");
-      counter = 0;
-    }
-
-    if (firstSelection === secondSelection){
-<<<<<<< HEAD
-=======
-
->>>>>>> 3d05fe6c924ccb76a0e563e9775112971e0acca6
-      const matchedCards = document.querySelectorAll(".clicked");
-      setTimeout(() =>{
-          for(let card of matchedCards){
-          card.remove();
-        }
-      }, 2000);
-    } else{
-<<<<<<< HEAD
-      const noMatch = document.querySelectorAll('.card.clicked');
-
-      setTimeout(() => {
-        noMatch[0].classList.remove('clicked');
-        noMatch[1].classList.remove('clicked');
-      },4000);
-    }
+const match = () => {
+  const selected = document.querySelectorAll('.selected');
+  selected.forEach(card => {
+    card.classList.add('match');
   });
-});
-=======
-          const matchedCards = document.querySelectorAll(
-        ".card[monument= ' + firstSelection + ' ]"
-      );
-      matchedCards[0].classList.add('matched');
-      matchedCards[0].classList.remove('clicked');
-      matchedCards[1].classList.add('matched');
-      matchedCards[1].classList.remove('clicked');
+};
 
-  
-    }else{
-            const noMatch = document.querySelectorAll('.card.clicked');
-            setTimeout(() => {
-              noMatch[0].classList.remove('clicked');
-              noMatch[1].classList.remove('clicked');
-            },4000);
-          }
-        });
-      });
-      $("#reset").unbind("click", cards);
->>>>>>> 3d05fe6c924ccb76a0e563e9775112971e0acca6
+const resetGuesses = () => {
+  firstGuess = '';
+  secondGuess = '';
+  count = 0;
+  previousTarget = null;
+
+  var selected = document.querySelectorAll('.selected');
+  selected.forEach(card => {
+    card.classList.remove('selected');
+  });
+};
+
+grid.addEventListener('click', event => {
+
+  const clicked = event.target;
+
+  if (
+    clicked.nodeName === 'SECTION' ||
+    clicked === previousTarget ||
+    clicked.parentNode.classList.contains('selected') ||
+    clicked.parentNode.classList.contains('match')
+  ) {
+    return;
+  }
+
+  if (count < 2) {
+    count++;
+    if (count === 1) {
+      firstGuess = clicked.parentNode.dataset.name;
+      console.log(firstGuess);
+      clicked.parentNode.classList.add('selected');
+    } else {
+      secondGuess = clicked.parentNode.dataset.name;
+      console.log(secondGuess);
+      clicked.parentNode.classList.add('selected');
+    }
+
+    if (firstGuess && secondGuess) {
+      if (firstGuess === secondGuess) {
+        setTimeout(match, delay);
+      }
+      setTimeout(resetGuesses, delay);
+    }
+    previousTarget = clicked;
+  }
+
+});
+
+
+
+
+
+
 
 
 
